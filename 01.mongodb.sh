00621b58d96e1c52919afb9dd6b833fc7ba71dac
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-03-31 12:26:26 +0530

    mongo 4

--- 01.mongodb.sh
+++ 01.mongodb.sh
@@ -27,12 +27,28 @@ validate(){
         exit 1
     else 
         echo -e "$G you're a root user $N"
-    fi
+    
 
     cp /home/centos/newrobo/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
     validate $? "copying mango.repo" 
 
     dnf install mongodb-org -y  &>> $LOGFILE
     validate $? "installing mongo-shell"
+
+    systemctl enable mongod &>> $LOGFILE
+    validate $? "enable mongod"
+
+    systemctl start mongod &>> $LOGFILE
+    validate $? "starting mongod"
+
+    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+    validate $? "remote access"
+
+    systemctl restart mongod &>> $LOGFILE
+    validate $? "restart mongod"
+
+    fi
+
+
 
 
