9dd7086667cb3526b81c0bcf1e1cc1cb2c9a2fdf
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-04-02 15:31:22 +0530

    robosh

--- 07.roboshop.sh
+++ 07.roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI=ami-0f3c7d07486cad139 #this keeps on changing
-SG_ID=sg-087e7afb3a936fce7 #replace with your SG ID
+SG_ID=sg-0ecc0d38b1974ad3f #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z07107583SIBMEYCNYDA6 # replace your zone ID
 DOMAIN_NAME="bigmatrix.in"
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0ecc0d38b1974ad3f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
