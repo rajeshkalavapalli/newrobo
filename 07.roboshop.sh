e8b55aa93f3a76c971fad673cc2604b162929769
rajeshkalavapalli <rajesh.kalavapalli@gmail.com>
2024-04-02 14:09:07 +0530

    instance through shell

---
+++ 07.roboshop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+
+AMI=ami-0f3c7d07486cad139
+SE_ID=sg-0ecc0d38b1974ad3f
+
+INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payment" "dispatch" "web")
+
+for i in  "${INSTANCE[@]}"
+do
+    echo "instance is: $i"
+   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "mongodb" ]
+   then 
+       INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-0ecc0d38b1974ad3f  
+
+done
